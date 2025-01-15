4c985e48e1662b269731fabf2a574c45127a9b05
Daniel Tran <129140506+DanielvTran@users.noreply.github.com>
2025-01-15 17:56:50 +1000

    nav as a component

--- project/src/app/pages/budgeting/page.tsx
+++ project/src/app/pages/budgeting/page.tsx
@@ -1,35 +1,12 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import { links } from "../../../../lib";
+// Components
+import Nav from "@/components/Nav";
 
 export default function Budgeting() {
-  const pathname = usePathname();
-
   return (
     <div className="welcome-container bg-base-200 min-h-screen flex flex-col lg:flex-row relative overflow-hidden">
-      <div className="left-container w-full lg:w-1/5 bg-[#323E42] flex justify-center min-h-screen py-20">
-        <div className="left-content">
-          <h1 className="heading text-[#98FF98] font-bold text-3xl lg:text-4xl mb-3 lg:mb-20">MintyPlan</h1>
-
-          <nav className="space-y-4 lg:space-y-8 xl:space-y-10 2xl:space-y-12 font-bold">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={`text-xl md:text-2xl lg:text-3xl block transition ${
-                  pathname === link.href
-                    ? "text-[#98FF98] border-r-4 border-[#98FF98] pr-2" // Active styles
-                    : "text-[#DFFFE2] hover:text-[#98FF98]"
-                }`}
-              >
-                {link.label}
-              </Link>
-            ))}
-          </nav>
-        </div>
-      </div>
+      <Nav />
 
       <div className="right-container w-full lg:w-4/5 bg-[#F2F2F2] flex min-h-screen py-20 px-20">
         <div className="max-w-sm lg:max-w-md xl:max-w-lg 2xl:max-w-2xl 3xl:max-w-3xl">
--- project/src/app/pages/categories/page.tsx
+++ project/src/app/pages/categories/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Link from "next/link";
-import { links } from "../../../../lib";
 import { useCategory } from "@/contexts/CategoriesContext";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { categorySchema } from "../../../../lib/validationSchema";
 import { ICategoriesForm } from "../../../../lib/types";
+
+// Components
+import Nav from "@/components/Nav";
 
 // Font Awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -87,27 +88,7 @@ export default function Categories() {
 
   return (
     <div className="welcome-container bg-base-200 min-h-screen flex flex-col lg:flex-row relative overflow-hidden">
-      <div className="left-container w-full lg:w-1/5 bg-[#323E42] flex justify-center min-h-screen py-20">
-        <div className="left-content">
-          <h1 className="heading text-[#98FF98] font-bold text-3xl lg:text-4xl mb-3 lg:mb-20">MintyPlan</h1>
-
-          <nav className="space-y-4 lg:space-y-8 xl:space-y-10 2xl:space-y-12 font-bold">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={`text-xl md:text-2xl lg:text-3xl block transition ${
-                  pathname === link.href
-                    ? "text-[#98FF98] border-r-4 border-[#98FF98] pr-2"
-                    : "text-[#DFFFE2] hover:text-[#98FF98]"
-                }`}
-              >
-                {link.label}
-              </Link>
-            ))}
-          </nav>
-        </div>
-      </div>
+      <Nav />
 
       <div className="right-container text-[#323E42] w-full lg:w-4/5 bg-[#F2F2F2] flex flex-col min-h-screen py-20 px-20">
         <h1 className=" font-bold text-3xl lg:text-4xl text-left mb-10">Personalise your categories!</h1>
